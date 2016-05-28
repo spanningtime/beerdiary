da70b9a6a18025bbda120667649602a5d674e083
William Knight <kniwilliam@gmail.com>
2016-05-27 18:15:34 -0700

    added functioning "Add Grains" button

--- app.js
+++ app.js
@@ -7,12 +7,27 @@
     selectYears: 15 // Creates a dropdown of 15 years to control year
   });
 
-  var $beerStyle = $('#beer_style');
-  $beerStyle.click(function() {
-    console.log($beerStyle.val());
-  })
+  var grainsArray = [];
+  var renderGrainsTable = function() {
+    var grainsInput = $('<div class="row"><div class="col 12"></div>\
+    <div class="input-field col s6"><input id="grainType" type="text">\
+    <label for="grainType">Type</label></div><div class="input-field col s6">\
+    <input id="grainAmount" type="text"><label for="grainAmount">\
+    Amount</label></div></div></div>');
 
-  var $xhr = $.getJSON()
+
+    $(".grainsInputBody").append(grainsInput);
+  };
+
+  var $addGrains = $('.addGrains');
+  $addGrains.click(function() {
+    grainsArray.push({Type: '', Amount: ''});
+    renderGrainsTable();
+
+  });
+
+
+
 
 
 })();
