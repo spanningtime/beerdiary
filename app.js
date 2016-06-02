eaee67265f5ee446f3886757f505b50e414a71ed
William Knight <kniwilliam@gmail.com>
2016-06-01 20:17:58 -0700

    added api functionality and styles

--- app.js
+++ app.js
@@ -2,33 +2,44 @@
 
   $(".dropdown-button").dropdown();
 
-  $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
+
+  $.ajaxSetup({
+    headers: {
+      'x-requested-with': 'whatever.com'
+    }
   });
 
-  var styleQuestion = $('<section class="center-align styleQuestion question">What style of beer are you making?<br><input class="styleQuestionInput"></input><br><a class="waves-effect waves-light btn styleQuestionButton">Next</a></section>');
 
-  var nameQuestion = $('<section class="center-align nameQuestion question">What do you want to name your beer?<br><input class="nameQuestionInput"></input><br><a class="waves-effect waves-light btn nameQuestionButton">Next</a></section>')
+
+  var styleQuestion = $('<section class="styleQuestion question">What style of beer are you making?<br><input class="styleQuestionInput"></input><br><a class="waves-effect waves-light btn styleQuestionButton">Next</a></section>');
+
+  var nameQuestion = $('<section class= nameQuestion question">What do you want to name your beer?<br><input class="nameQuestionInput"></input><br><a class="waves-effect waves-light btn nameQuestionButton">Next</a></section>')
 
   var $grainsQuestion = $('<section class="center-align grainsQuestion question">Enter your grain bill.</section>')
 
   var $mainArea = $('.mainArea');
 
-  var addGrainsButton = $('<div class="addGrainsButton"><a class="addGrainsButton btn-floating"><i class=" material-icons">\
-  add</i></a></div>');
+  var addGrainsDoneButton = $('<div class="row marginBottom"><div class="col s6"><a class="addGrainsButton btn-floating"><i class=" material-icons">\
+      add</i></a></div><div class="col s6"><a class="doneGrainsButton\
+      waves-effect waves-light btn right-align">Done With Grains</a></div></div>');
 
-  var doneGrainsButton = $('<div><a class="doneGrainsButton waves-effect waves-light btn">Done</a></div>');
+  var doneGrainsButton = $('<a class="doneGrainsButton waves-effect waves-light btn">Done With Grains</a>');
 
-  var $addHopsButton = $('<div class="addHopsButton"><a class="addHopsButton btn-floating"><i class=" material-icons">\
-  add</i></a></div>');
+  var $addHopsButton = $('<a class="addHopsButton btn-floating"><i class=" material-icons">add</i></a>');
 
-  var $doneHopsButton = $('<div><a class="doneHopsButton waves-effect waves-light btn">Done</a></div>');
+  var $doneHopsButton = $('<a class="doneHopsButton waves-effect waves-light btn">Done With Hops</a>');
 
-  var $hopsQuestion = $('<div class="col s12 hopsQuestion question"<section class="center-align hopsQuestion question">Enter your hop bill.</section></div>')
+  var $hopsQuestion = $('<div class="col s12 hopsQuestion question center-align"<section class=hopsQuestion question">Enter your hop bill.</section></div>')
 
-  var $sessionBeerDetails = $('<div class="row"><div id="brewName" class="col s4 center-align" ></div>\
-      <div id="beerStyle"class="col s4 center-align"></div><div class="col s4 center-align" id="date"></div></div>');
+  var $sessionBeerDetails = $('<div class="row"><div id="brewName" class="col s4 center-align sessionDetails" ></div>\
+      <div id="beerStyle"class="col s4 center-align sessionDetails"></div>\
+      <div class="col s4 center-align sessionDetails" id="date"></div></div>');
+
+  var $beerListSelect = $('<div class="col s12 beerListSelect">\
+    <h1>Select the correct beer style</h1>\
+    <ul id="#beerList"></ul></div>');
+
+  var $beerListUl = $beerListSelect.children('ul')
 
   var $inputField = $('.input-field');
 
@@ -40,8 +51,18 @@
 
   var hopsArray = [];
 
+  var beerStyleArray = [];
+
+  var beerStyleListArray = [];
+
+  var $belowMainArea = $('.belowMainArea');
+
+  var beerId = '';
+
+  var beerStyleData = [];
+
   var generateRightColumn = function() {
-    $mainArea.append($('<div class="row"><div class="col s6"><div class="row">\
+    $belowMainArea.append($('<div class="row"><div class="col s6"><div class="row">\
       <div class="col s12"><h5>Mash Schedule</h5></div></div></div>\
       <a class="mashCalc" href="https://www.brewersfriend.com/mash">Mash Calculator</a><div class="row"><div class="col s6"><div class="row">\
       <div class="col s12"><div class="input-field col s12"><input id="type"\
@@ -65,11 +86,16 @@
       <label for="hops">Hops</label></div></div><div><div class="input-field col\
        s6"><input id="yeast" type="text"><label for="yeast">Yeast</label></div>\
       </div><div><div class="input-field col s6"><input id="other" type="text">\
-      <label for="other">Other</label></div></div></div></div></div></div>'));
+      <label for="other">Other</label></div></div></div></div></div></div>')).hide().fadeIn(800);
   }
 
   var generateLeftColumn = function() {
-    $mainArea.append($('<div class="col s6"><div class="row"><div class="col s12"><h5>BatchStats</h5></div></div><div class="row">\
+    beer.targetOG = $('#targetOG').val();
+    beer.targetFG = $('#targetFG').val();
+    beer.abv = $('#abv').val();
+    beer.ibu = $('#ibu').val();
+    beer.preBoil = $('#preBoil').val();
+    $belowMainArea.append($('<div class="col s6"><div class="row"><div class="col s12"><h5>BatchStats</h5></div></div><div class="row">\
     <div class="col s12"><div class="input-field col s6"><input id="targetOG"\
     type="text"><label for="targetOG">Target Original Gravity</label>\
     </div><div class="input-field col s6"><input id="targetFG" type="text">\
@@ -90,7 +116,7 @@
      <div class="col s12"><h5>Other Notes</h5></div></div><div class="row">\
      <form class="col s12"><div class="row"><div class="input-field col s12">\
      <textarea id="textarea1" class="materialize-textarea"></textarea></div>\
-     </div></form></div></div>'));
+     </div></form></div></div>')).hide().fadeIn(800);
   };
 
   var addHopsToTable = function() {
@@ -107,8 +133,6 @@
     }
     generateLeftColumn();
     generateRightColumn();
-    console.log(hopsObject);
-    console.log(hopsArray);
   }
 
 // ****** FIRST ROW ******
@@ -159,12 +183,12 @@
     $mainArea.append($hopsQuestion);
     addHopsRow();
     appendHopsAddDoneButtons();
-    $addHopsButton.click(addHopsRow)
+    $('.addHopsButton').click(addHopsRow)
     $doneHopsButton.click(hideHopsRowAndQuestion);
   }
 
   var appendGrainsAddDoneButtons = function() {
-    $mainArea.append(addGrainsButton, doneGrainsButton);
+    $mainArea.append(addGrainsDoneButton);
   }
 
   var addGrainsFieldAndButtons = function() {
@@ -212,35 +236,105 @@
   }
 
   var loadNewGrainsFieldAndButtons = function() {
-    addGrainsButton.click(addGrainsFieldAndButtons);
+    $('.addGrainsButton').click(addGrainsFieldAndButtons);
   }
 
-  var loadGrainsQuestion = function() {
+  var createModalContent = function() {
+    $('.modal-content').append($('<h4>' + beer.style + '</h4><p>' + beerStyleData[0].description + '</p><div class="col s6">Minimum ABV: ' + beerStyleData[0].abvMin + '%</div><div class="col s6">Maximum ABV: ' + beerStyleData[0].abvMax + '%</div><div class="col s6">Minimum Final Gravity: ' + beerStyleData[0].fgMin + '</div><div class="col s6">Maximum Final Gravity: ' + beerStyleData[0].fgMax + '</div><div class="col s6">Minimum IBUs: ' + beerStyleData[0].ibuMin + '</div><div class="col s6">Maximum IBUs: ' + beerStyleData[0].ibuMax + '</div>'));
+    console.log(beerStyleData)
+  };
+
+  var startXHR2 = function() {
+    var $xhr = $.getJSON('http://cors-anywhere.herokuapp.com/api.brewerydb.com/v2/style/' + beerId + '?key=90d1c30e19a750d723e5318a32f983f3');
+
+    $xhr.done(function(data) {
+      var obj = {};
+      obj.description = data.data.description;
+      obj.abvMin = data.data.abvMin;
+      obj.abvMax = data.data.abvMax;
+      obj.fgMin = data.data.fgMin;
+      obj.fgMax = data.data.fgMax;
+      obj.ibuMin = data.data.ibuMin;
+      obj.ibuMax = data.data.ibuMax;
+      beerStyleData.push(obj);
+      createModalContent();
+      console.log(obj);
+    });
+  }
+
+
+  var loadGrainsQuestionAndSessionDetails = function() {
+    startXHR2();
+    // $('body').css("background", "white").fadeIn(5000);
     beer.name = $('.nameQuestionInput').val()
     $('.nameQuestion').hide();
     $mainArea.prepend($sessionBeerDetails);
-    $('#brewName').append('<h4> Name: ' + beer.name + '</h4>')
-    $('#beerStyle').append('<h4> Style: ' + beer.style + '</h4>')
-    $('#date').append('<h4>' + (beer.date.getMonth() + 1) + "/" + beer.date.getDate() + "/" + beer.date.getFullYear() + '</h4>')
-    $mainArea.append($grainsQuestion).hide().fadeIn(2000);
+    $('#brewName').append('<h4>' + beer.name + '</h4>')
+    $('#beerStyle').append('<h4><a class="modal-trigger" href="#modal1">' + beer.style + '</a></h4>')
+    $('.modal-trigger').leanModal();
+    $('#date').append('<h4>' + (beer.date.getMonth() + 1) + "." + beer.date.getDate() + "." + beer.date.getFullYear() + '</h4>')
+    $mainArea.append($grainsQuestion).hide().fadeIn(1000);
     addGrainsFieldAndButtons();
     loadNewGrainsFieldAndButtons();
-    doneGrainsButton.click(hideGrainRowAndQuestion)
+    $('.doneGrainsButton').click(hideGrainRowAndQuestion)
+  }
+
+  var loadNameQuestion = function(event) {
+    beer.style = $(event.target).text();
+    for (var x = 0; x < beerStyleArray.length; x++) {
+      if (beerStyleArray[x].name === beer.style) {
+        beerId = beerStyleArray[x].id;
+        break;
+      }
+    }
+    $beerListSelect.hide();
+    $('.styleQuestion').hide();
+    $mainArea.append(nameQuestion).hide().fadeIn(1000);
+    $('.nameQuestionButton').click(loadGrainsQuestionAndSessionDetails);
+    console.log(beerId);
+  }
+
+  var selectBeerListItem = function() {
+    $('.beerListItem').click(loadNameQuestion)
+
+  }
+
+  var createBeerListSelect = function() {
+    for (var x = 0; x < beerStyleArray.length; x++) {
+      $beerListUl.append('<li class="beerListItem">' + beerStyleArray[x].name + '</li>')
+    }
+    $mainArea.append($beerListSelect);
+
+    selectBeerListItem();
+  }
+
+  var startXHR = function() {
+    $('.styleQuestionButton').hide();
+    beer.style = $('.styleQuestionInput').val();
+    var $xhr = $.getJSON('http://cors-anywhere.herokuapp.com/api.brewerydb.com/v2/search/style?q=' + beer.style + '&key=90d1c30e19a750d723e5318a32f983f3');
+
+    $xhr.done(function(data) {
+      if ($xhr.status !== 200) {
+          return;
+      }
+      for (var x = 0; x < data.data.length; x++) {
+        var obj = {}
+        obj.name = data.data[x].name
+        obj.id = data.data[x].id
+        beerStyleArray.push(obj);
+      }
+      console.log(beerStyleArray);
+      createBeerListSelect();
+    });
   }
 
 
-  var loadNameQuestion = function() {
-      beer.style = $('.styleQuestionInput').val();
-      $('.styleQuestion').hide();
-      $mainArea.append(nameQuestion).hide().fadeIn(2000);
-      $('.nameQuestionButton').click(loadGrainsQuestion);
-    }
 
   var loadStyleQuestion = function() {
     // Append the Style Questions HTML to the page
-    $mainArea.append(styleQuestion).hide().fadeIn(2000);
+    $mainArea.append(styleQuestion).hide().fadeIn(1000);
     // Establish event listener for new button
-    $('.styleQuestionButton').click(loadNameQuestion);
+    $('.styleQuestionButton').click(startXHR);
   }
 
   var hideStartSession = function() {
