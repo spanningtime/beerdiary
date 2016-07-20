2dd655ffa943321173b45ff67c44f5dafc86f908
William Knight <kniwilliam@gmail.com>
2016-07-20 13:28:01 -0700

    Close #4 Add users migration

---
+++ migrations/20160719134342_users.js
@@ -0,0 +1,19 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('first_name')
+      .notNullable()
+      .defaultTo('');
+    table.string('last_name')
+      .notNullable()
+      .defaultTo('');
+    table.specificType('hashed_password', 'char(60)')
+      .notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
